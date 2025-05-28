bb0c7936eaa4eff4004c2e9881feb930389cc215
Aldiyar <raimov.aldiyar@mail.ru>
2025-05-28 12:55:05 +0500

    Switch DB

--- user-management-back/server.js
+++ user-management-back/server.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -8,10 +8,21 @@ const PORT = process.env.PORT || 5000;
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
+
+db.getConnection()
+  .then((conn) => {
+    console.log("✔ Connected to remote MySQL");
+    conn.release();
+  })
+  .catch((err) => {
+    console.error("❌ MySQL Connection Error:", err.message);
+    process.exit(1);
+  });
 
 const authRoutes = require("./routes/auth")(db);
 const userRoutes = require("./routes/users")(db);
