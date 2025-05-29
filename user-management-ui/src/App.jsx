a3df28ca0c2dae26ae20e899ff58c7e31034e3fc
Aldiyar <raimov.aldiyar@mail.ru>
2025-05-29 22:18:23 +0500

    bug fix

--- user-management-ui/src/App.jsx
+++ user-management-ui/src/App.jsx
@@ -1,26 +1,56 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import UsersPage from "./pages/UsersPage";
 
-function App() {
+function AppRoutes() {
   const isAuthenticated = !!localStorage.getItem("token");
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route
-          path="/users"
-          element={
-            isAuthenticated ? <UsersPage /> : <Navigate to="/login" replace />
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      <Route
+        path="/login"
+        element={
+          !isAuthenticated ? <LoginPage /> : <Navigate to="/users" replace />
+        }
+      />
+
+      <Route
+        path="/register"
+        element={
+          !isAuthenticated ? <RegisterPage /> : <Navigate to="/users" replace />
+        }
+      />
+
+      <Route
+        path="/users"
+        element={
+          isAuthenticated ? <UsersPage /> : <Navigate to="/login" replace />
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
 }
 
-export default App;
+function App() {
+  useLocation();
+  return <AppRoutes />;
+}
+
+export default function Root() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
